3b66368293cc4baf6530096133cfa15ff77e1de6
a10315 <s.wang@shinesoft.co.jp>
2021-08-23 10:19:41 +0900

    Delete k8s directory

--- k8s/ebs.tf
+++
@@ -1,8 +0,0 @@
-# ------------------
-# create ks8_ebs
-# ------------------
-resource "aws_ebs_volume" "k8s_ebs" {
-  availability_zone = "ap-northeast-1d"
-  size              = 3
-
-}
--- k8s/ssm.tf
+++
@@ -1,8 +0,0 @@
-# --------------------------
-# update ebs id
-# --------------------------
-resource "aws_ssm_parameter" "ebs_id" {
-  name  = "ebs_id"
-  type  = "String"
-  value = aws_ebs_volume.k8s_ebs.id
-}
